4a868a4498c5879691377e4cfedbfa27950fcca0
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2024-01-04 11:07:41 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 356 ms, Memory: 8292 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	priority_queue <int, vector<int>, greater<int>> q;
+
+	for (int i = 0; i < N; i++) {
+		int temp;
+		cin >> temp;
+		q.push(temp);
+	}
+
+	for (int i = 0; i < N; i++) {
+		int temp;
+		temp = q.top();
+		q.pop();
+		cout << temp << "\n";
+	}
+
+}
