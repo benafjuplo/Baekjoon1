f5d514a78e94ca10632d855df4cd893b8aec6f3f
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2024-01-23 06:26:00 +0900

    [Silver IV] Title: 스택, Time: 344 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<stack>
+#include<string>
+
+using namespace std;
+
+int main() {
+	stack <int> s;
+	int t;
+	cin >> t;
+	string temp;
+	getline(cin, temp);
+
+	for (int i = 0; i < t; i++) {
+		string str;
+		getline(cin, str);
+
+		//push
+		if (str.find("push") != string::npos) {
+			string n = str.substr(4);
+			int p = stoi(n);
+			s.push(p);
+			continue;
+		}
+
+		//pop
+		if (str.find("pop") != string::npos) {
+			if (s.empty())
+				cout << -1 << "\n";
+			else {
+				cout << s.top() << "\n";
+				s.pop();
+			}
+			continue;
+		}
+
+		//size
+		if (str.find("size") != string::npos) {
+			cout << s.size() << "\n";
+			continue;
+		}
+
+		//empty
+		if (str.find("empty") != string::npos) {
+			if (s.empty())
+				cout << 1 << "\n";
+			else
+				cout << 0 << "\n";
+			continue;
+		}
+
+		//top
+		if (str.find("top") != string::npos) {
+			if (s.empty())
+				cout << -1 << "\n";
+			else
+				cout << s.top() << "\n";
+			continue;
+		}
+	}
+}
