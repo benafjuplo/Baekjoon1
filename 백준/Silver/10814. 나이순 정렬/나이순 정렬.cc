cddd6717186e7c6b2f8c5d51f1a9999aa3fac538
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2024-01-25 20:25:52 +0900

    [Silver V] Title: 나이순 정렬, Time: 132 ms, Memory: 11256 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<queue>
+#include<string>
+#include<vector>
+using namespace std;
+
+struct user{
+	int age;
+	string name;
+	int seq;
+	user(int a, string n, int s) {
+		age = a;
+		name = n;
+		seq = s;
+	}
+};
+
+struct compare {
+	//is a greater than b ?
+	bool operator()(user a, user b) {
+		if (a.age != b.age) {
+			if (a.age > b.age) {
+				return true;
+			}
+			else return false;
+		}
+		else {
+			if (a.seq > b.seq) {
+				return true;
+			}
+			else return false;
+		}
+	}
+};
+
+int main() {
+	int n;
+	cin >> n;
+	priority_queue<user, vector<user>, compare> p;
+	for (int i = 0; i < n; i++) {
+		int age;
+		string name;
+		cin >> age;
+		cin >> name;
+		p.push(user(age, name, i));
+	}
+	
+	for (int i = 0; i < n; i++) {
+		cout << p.top().age << " " << p.top().name << "\n";
+		p.pop();
+	}
+}
