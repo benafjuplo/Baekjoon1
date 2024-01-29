1cfc4d20543f3beffd779b34c15c73cc24d9c5ad
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2024-01-30 07:06:56 +0900

    [Silver V] Title: 영화감독 숌, Time: 44 ms, Memory: 2536 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -1,6 +1,34 @@
 #include<iostream>
 #include<string>
+#include<iterator>
+#include<algorithm>
+#include<vector>
+#include<cmath>
 using namespace std;
+
+vector<int> box;
+vector<string> v;
+void select(int digit) {
+	for (int i = 0; i < 10; i++) {
+		if (digit == 0 && i==0) {
+			continue;
+		}
+		if (v[digit] != "666")
+			v[digit] = to_string(i);
+		else
+			i = 10;
+		if(digit != v.size()-1)
+			select(digit + 1);
+		else {
+			string str = "";
+			for (int j = 0; j < v.size(); j++)
+				str += v[j];
+			box.push_back(stoi(str));
+		}
+	}
+
+	return;
+}
 
 int main() {
 	cin.tie(NULL);
@@ -9,17 +37,42 @@ int main() {
 
 	int N;
 	cin >> N;
-	int seq = 666;
-	for (int i = 1; i < N; ) {
-		seq++;
-		string seqS = to_string(seq);
-		if (seqS.find("666") == string::npos) {
-			continue;
-		}
-		else {
-			i++;
-		}
-	}
 
-	cout << seq;
+	int digit = 3;
+	string str = "666";
+
+	for (int i = 1; i < N;) {
+
+		digit++;
+
+		//Initialize
+		v.clear();
+		for (int j = 0; j <= digit-3; j++) {
+			string temp = "1";
+			v.push_back(temp);
+		}
+				
+		// Select
+		for (int j = 0; j <= digit-3; j++) {
+			if (j > 0)
+				v[j - 1] = "1";
+			v[j] = "666";
+			select(0);
+		}
+
+		// Sort
+		sort(box.begin(), box.end());
+		box.erase(unique(box.begin(), box.end()), box.end());
+		while(!box.empty()){
+			str = to_string(box[0]);
+			box.erase(box.begin());
+			i++;
+			if (i >= N)
+				break;
+		}
+		if (i >= N)
+			break;
+	}
+	
+	cout << str;
 }
