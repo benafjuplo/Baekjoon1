7444a683fa0349f1ab721974413deab79d975171
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2024-03-26 06:18:45 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int T;
+	cin >> T;
+
+	for (int i = 0; i < T; i++) {
+		string str;
+		cin >> str;
+		cout << str[0] << str[str.size() - 1] << "\n";
+	}
+}
+
+//Finish
