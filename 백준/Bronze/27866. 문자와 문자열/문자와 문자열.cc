aeff9029fc7da9a0c32927882f3c971e75f5f4ca
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2024-03-27 04:41:59 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+
+using namespace std; 
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	string S;
+	cin >> S;
+	int i;
+	cin >> i;
+
+	cout << S[i-1];
+}
+
+//Finish
