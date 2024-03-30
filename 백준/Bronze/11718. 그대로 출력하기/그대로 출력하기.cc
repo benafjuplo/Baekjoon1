d630e46c52a3eb13e90c90d6344f096ce2bd83fe
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2024-03-30 14:41:31 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	string str;
+
+	while (getline(cin, str)) {
+		cout << str << "\n";
+	}
+
+}
+
+//Finish
