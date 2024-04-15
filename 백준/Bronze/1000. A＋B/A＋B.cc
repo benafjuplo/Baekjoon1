6749098965c678b13cfa1f833e6765bdc4808bc4
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2024-04-15 19:11:15 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,19 @@
+/*1000 - A+B*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int A, B;
+	cin >> A >> B;
+
+	cout << A + B;
+}
+
+
+//Finsih
