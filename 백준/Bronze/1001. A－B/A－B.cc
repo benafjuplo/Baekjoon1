f341c70bc68ebeb087dc2ab46edefdf2bd63268d
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2024-04-18 00:25:27 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,18 @@
+/* 1001 - A-B */
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+
+	int A, B;
+	cin >> A >> B;
+
+	cout << A - B;
+}
+
+//Finish
