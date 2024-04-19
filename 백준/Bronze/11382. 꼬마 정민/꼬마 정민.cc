7e11432d8540ab591b2a8874136e28f843cfc7e1
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2024-04-19 15:04:28 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,18 @@
+/* 11382 - Little Jeongmin */
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	unsigned long long A, B, C;
+	cin >> A >> B >> C;
+
+	cout << A + B + C;
+}
+
+//Finish
