509be68872af43a1f96d03f3d2f8b865d2b9211b
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2024-05-10 06:12:45 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -0,0 +1,23 @@
+/*
+- Title : Cutting Chocolate
+- Start Time : 24-05-09 06:33
+- End Time : 24-05-09 06:36
+- Try : 1
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int N, M;
+	cin >> N >> M;
+
+	cout << N * M - 1;
+}
+
+//Finish
