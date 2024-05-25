45b80621a4c832c0fadf9e7a763abd9197ba028d
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2024-05-26 08:34:09 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,22 @@
+/*
+- Title : 27323 - Rectangle
+- Start Time : 24-05-21, 16:26
+- End Time : 24-05-21, 16:27
+- Try : 1
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int A, B;
+    cin >> A >> B;
+    cout << A * B;
+}
+
+//Finish
