fd04bf0c826d0ad76c36b28e95cf0d2d8dd4c37f
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2024-07-01 14:45:15 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,24 @@
+/*
+- Title : 24263 - Algorithm Class - Execution Time of Algorithms 2
+- Start Time : 24-07-01, 10:58
+- End Time : 24-07-01, 11:03
+- Try : 1
+- Category : Simple, Basic, I/O
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+
+	cout << n << "\n" << 1 << "\n";
+}
+
+//Finish
