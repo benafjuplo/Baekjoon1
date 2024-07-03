0ac6e98bb469d145bd4f1010d0ac5346a0421150
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2024-07-03 10:30:28 +0900

    [Bronze V] Title: Hook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.cc
@@ -0,0 +1,24 @@
+/*
+- Title : 10189 - Hook
+- Start Time : 24-07-03, 09:47
+- End Time : 24-07-03, 09:50
+- Try : 1
+- Category : I/O, Simple, Basic
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	cout.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	cout << "#  # #### #### #  #\n";
+	cout << "#### #  # #  # # #\n";
+	cout << "#### #  # #  # # #\n";
+	cout << "#  # #### #### #  #\n";
+}
+
+//Finish
