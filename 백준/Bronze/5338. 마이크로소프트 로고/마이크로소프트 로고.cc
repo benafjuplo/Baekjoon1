302253df64e68c165bdaf1cc4fcc918afdf5aecd
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2024-07-14 05:04:53 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,25 @@
+/*
+- Title : 5338 - Microsoft Logo
+- Start Time : 24-07-14, 04:32
+- End Time : 24-07-14, 04:35
+- Try : 1
+- Category : I/O, Basic, Simple
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	cout.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	cout << "       _.-;;-._" << "\n";
+	cout << "'-..-'|   ||   |" << "\n";
+	cout << "'-..-'|_.-;;-._|" << "\n";
+	cout << "'-..-'|   ||   |" << "\n";
+	cout << "'-..-'|_.-''-._|" << "\n";
+}
+
+//Finish
