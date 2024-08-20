a26c6572099b7f9bb2be5149cb046ae548533462
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2024-08-21 00:31:54 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,22 @@
+/*
+- Title : 7287 - Registration
+- Start Time : 24-08-21. 00:28
+- End Time : 24-08-21. 00:30
+- Try : 1
+- Category : cout, print, basic, simple
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	cout.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	cout << 326 << "\n";
+	cout << "benafjuplo";
+}
+
+//Finish
