3ed233f7a0ab4b2f1ddc465244ace086783d4873
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2024-08-22 00:10:12 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,33 @@
+/*
+- Title : 2393 - Rook
+- Start Time : 24-08-22, 00:05
+- End Time : 24-08-22, 00:09
+- Try : 1
+- Category : Basic, cout, print, simple
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	cout.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	cout << "  ___  ___  ___\n";
+	cout << "  | |__| |__| |\n";
+	cout << "  |           |\n";
+	cout << "   \\_________/\n";
+	cout << "    \\_______/\n";
+	cout << "     |     |\n";
+	cout << "     |     |\n";
+	cout << "     |     |\n";
+	cout << "     |     |\n";
+	cout << "     |_____|\n";
+	cout << "  __/       \\__\n";
+	cout << " /             \\\n";
+	cout << "/_______________\\";
+}
+
+//Finish
