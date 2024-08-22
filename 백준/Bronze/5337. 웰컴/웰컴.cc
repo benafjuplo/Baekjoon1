90b848f12fbfed9494bc18cfa690b752b783bd60
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2024-08-22 14:46:36 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,23 @@
+/*
+- Title : 5337 - Welcome
+- Start Time : 24-08-22, 14:43
+- End Time : 24-08-22, 14:45
+- Try : 1
+- Category : Basic, cout, print, simple
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	cout.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	cout << ".  .   .\n";
+	cout << "|  | _ | _. _ ._ _  _\n";
+	cout << "|/\\|(/.|(_.(_)[ | )(/.\n";
+}
+
+//Finish
