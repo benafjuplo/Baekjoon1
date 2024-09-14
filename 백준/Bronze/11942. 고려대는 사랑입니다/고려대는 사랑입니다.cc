cb057001bda7f4b49653d771021433a583c88ef6
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2024-09-15 02:59:32 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.cc
@@ -0,0 +1,55 @@
+/*
+- Title : 11942 - Korea University is Love
+- Start Time : 24-09-15, 02:45
+- End Time : 24-09-15, 02:46
+- Try : 1
+- Category : basic, I/O, simple
+*/
+
+/* 한국어 문제 요약
+- 문제
+"고려대학교"를 출력하시오.
+
+- 출력
+첫째 줄에 "고려대학교"를 출력하시오.
+
+- 예제
+출력 :
+고려대학교
+*/
+
+/* 한국어 주석
+이 문제는 단순히 "고려대학교"라는 문자열을 출력하는 간단한 문제이다.
+하지만 하드웨어를 통한 출력을 직접 다루거나, 한국어가 아닌 환경에서 "고려대학교"를 출력하는 경우와 같은 특별한 상황에서는 난이도가 증가할 수 있다.
+*/
+
+/* English Problem Summary
+- Question
+Print "고려대학교".
+
+- Output
+Print "고려대학교" on the first line.
+
+- Example
+Output :
+고려대학교
+*/
+
+/* English Comment
+This problem is a simple one that requires printing the string "고려대학교".
+However, in special cases such as dealing with direct hardware-based output or printing "고려대학교" in non-Korean environments, the difficulty may increase.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	cout.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	cout << "고려대학교";
+}
+
+//Finish
