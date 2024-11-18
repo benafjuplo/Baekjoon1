11c070c7d67afdc23ed94a35a632016bf141608b
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2024-11-18 21:23:34 +0900

    [Bronze V] Title: 새로운 시작, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.cc
@@ -0,0 +1,52 @@
+/*
+- Title : 15962 - New Start
+- Start Time : 24-11-18, 21:18
+- End Time : 24-11-18, 21:19
+- Try : 1
+- Category : simple, basic
+- Language : C++14
+*/
+
+/* 한국어 문제 요약
+- 문제
+"파이팅!!"을 출력한다.
+
+- 입력
+입력은 없다.
+
+- 출력
+"파이팅!!"을 출력한다.
+*/
+
+/* 한국어 주석
+단순히 "파이팅!!"을 출력하면 된다.
+*/
+
+/* English Problem Summary
+- Problem
+Print "파이팅!!".
+
+- Input
+There is no input.
+
+- Output
+Print "파이팅!!".
+*/
+
+/* English Comment
+Simply print "파이팅!!".
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	//cout.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	cout << "파이팅!!";
+
+	return 0;
+}
