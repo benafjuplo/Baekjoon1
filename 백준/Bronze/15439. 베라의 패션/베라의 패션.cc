39d3a0118e2e8d62f65ee495838b8c40b350e17e
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-01-12 03:02:29 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,30 @@
+/*
+- Title : 15439 - Vera and Outfits
+- Start Time : 25-01-12, 02:59
+- End Time : 25-01-12, 03:00
+- Try : 1
+- Category : mathematics, simple, basic, combinatorics
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	//cout.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+	cout << N * (N - 1);
+
+	return 0;
+}
+
+//Finish
