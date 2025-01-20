d6caa101021b27c5d9d6299e301284ac2a0717c0
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-01-20 20:40:07 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.cc
@@ -0,0 +1,29 @@
+/*
+- Title : 8871 - Test Task 2
+- Start Time : 25-01-20, 20:37
+- End Time : 25-01-20, 20:39
+- Try : 1
+- Category : mathematics, simple, basic, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	//cout.tie(0);
+	ios_base::sync_with_stdio(false);
+	
+	int n;
+	cin >> n;
+	cout << (n + 1) * 2 << " " << (n + 1) * 3;
+
+	return 0;
+}
+
+//Finish
