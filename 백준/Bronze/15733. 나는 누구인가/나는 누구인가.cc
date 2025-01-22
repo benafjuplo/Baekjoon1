28f9012982ccfbfc4781e4efbe562b72d3701708
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-01-22 23:06:47 +0900

    [Bronze V] Title: 나는 누구인가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.cc
@@ -0,0 +1,28 @@
+/*
+- Title : Who am I
+- Start Time : 25-01-22, 22:48
+- End Time : 25-01-22, 22:57
+- Try : 1
+- Category : simple, basic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	//cout.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	cout << "I'm Sexy";
+
+	return 0;
+}
+
+//Finish
