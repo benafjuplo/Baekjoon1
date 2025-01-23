c513bf2c242feff364fce4ce8119b60a18f75b21
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-01-23 23:17:59 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,30 @@
+/*
+- Title : 32384 - Love is Korea University
+- Start Time : 25-01-23, 22:59
+- End Time :
+- Try : 1
+- Category : simple, basic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	//cout.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+	while (N--) cout << "LoveisKoreaUniversity ";
+
+	return 0;
+}
+
+//Finish
