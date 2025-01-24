e755909fa7a244b735558e1e6bf6276f8c143ca3
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-01-25 01:28:45 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.cc
@@ -0,0 +1,30 @@
+/*
+- Title : 16170 - What is today’s date?
+- Start Time : 25-01-25, 01:26
+- End Time : 25-01-25, 01:27
+- Try : 1
+- Category : simple, basic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	//cout.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	cout << "2025\n";
+	cout << "01\n";
+	cout << "24";
+
+	return 0;
+}
+
+//Finish
