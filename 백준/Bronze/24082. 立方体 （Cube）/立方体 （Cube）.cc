be8f8014a85599a3b842b08b3187c5e9a3cff79f
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-03-03 02:54:02 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.cc
@@ -0,0 +1,30 @@
+/*
+- Title : 24082 - Cube
+- Start Time : 25-03-03, 02:52
+- End Time : 25-03-03, 02:53
+- Try : 1
+- Category : simple, basic, mathematics, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	//cout.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	int x;
+	cin >> x;
+	cout << x * x * x;
+
+	return 0;
+}
+
+//Finish
