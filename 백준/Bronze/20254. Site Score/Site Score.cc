6950492effd0bd99249c1d329680505c6d779b25
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-03-04 00:50:53 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.cc
@@ -0,0 +1,31 @@
+/*
+- Title : 20254 - Site Score
+- Start Time : 25-03-04, 00:38
+- End Time : 25-03-04, 00:39
+- Try : 1
+- Category : simple, basic, mathematics, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	//cout.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+
+	cout << 56 * a + 24 * b + 14 * c + 6 * d;
+
+	return 0;
+}
+
+//Finish
