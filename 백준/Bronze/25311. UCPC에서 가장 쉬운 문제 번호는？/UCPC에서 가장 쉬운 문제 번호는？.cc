b57798a4e20ab7698f1a3ce3ffdf13d244d9d7f4
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-03-13 20:52:37 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.cc
@@ -0,0 +1,28 @@
+/*
+- Title : 25311 - What is the easiest problem number in UCPC?
+- Start Time : 25-03-13, 20:50
+- End Time : 25-03-13, 20:51
+- Try : 1
+- Category : simple, basic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	//cout.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	cout << "A";
+
+	return 0;
+}
+
+//Finish
