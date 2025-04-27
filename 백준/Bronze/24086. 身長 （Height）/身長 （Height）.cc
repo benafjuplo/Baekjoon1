b7ee85dbbcec4ccc615263de515b539dcf11c35a
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-04-28 01:09:37 +0900

    [Bronze V] Title: 身長 (Height), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.cc
@@ -0,0 +1,29 @@
+/*
+- Title : 24086 - Height
+- Start Time : 25-04-28, 01:07
+- End Time : 25-04-28, 01:08
+- Try : 1
+- Category : mathematics, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	int a, b;
+	cin >> a >> b;
+	cout << b - a;
+
+	return 0;
+}
+
+// Finish
