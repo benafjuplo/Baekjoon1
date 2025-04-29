dcd8a6e68304e8ef527129828823427dd0b911e0
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-04-29 22:51:28 +0900

    [Bronze V] Title: 時間 (Hour), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.cc
@@ -0,0 +1,31 @@
+/*
+- Title : 27327 - Hour
+- Start Time : 25-04-29, 22:48
+- End Time : 25-04-29, 22:50
+- Try : 1
+- Category : simple, basic, mathematics, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	int x;
+	cin >> x;
+
+	cout << x * 24;
+
+	return 0;
+}
+
+// Finish
