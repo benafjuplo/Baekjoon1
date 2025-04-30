f9c167387774f09f1d66e9f9b2662b1b2695bed6
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-05-01 00:09:55 +0900

    [Bronze V] Title: ゾロ目 (Same Numbers), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27324. ゾロ目 （Same Numbers）/ゾロ目 （Same Numbers）.cc
@@ -0,0 +1,31 @@
+/*
+- Title : 27324 - Same Numbers
+- Start Time : 25-04-29, 22:57
+- End Time : 25-04-29, 22:59
+- Try : 1
+- Category : simple, basic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	char a, b;
+	cin >> a >> b;
+
+	cout << (a == b);
+
+	return 0;
+}
+
+// Finish
