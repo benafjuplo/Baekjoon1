b305753320341c2390b74c0358c8a847d6cec1c7
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-05-04 00:28:56 +0900

    [Bronze V] Title: 2 桁の整数 (Two-digit Integer), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27331. 2 桁の整数 （Two－digit Integer）/2 桁の整数 （Two－digit Integer）.cc
@@ -0,0 +1,31 @@
+/*
+- Title : 27331 - Two-digit Integer
+- Start Time : 25-05-04, 00:26
+- End Time : 25-05-04, 00:27
+- Try : 1
+- Category : simple, basic, mathematics, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	int a, b;
+	cin >> a >> b;
+
+	cout << a * 10 + b;
+
+	return 0;
+}
+
+// Finish
