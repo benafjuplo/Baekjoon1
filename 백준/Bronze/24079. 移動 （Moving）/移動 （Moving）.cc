dd0e8830b6c64836abcac002900c8b686699ed53
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-05-18 22:03:58 +0900

    [Bronze IV] Title: 移動 (Moving), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24079. 移動 （Moving）/移動 （Moving）.cc
@@ -0,0 +1,31 @@
+/*
+- Title : 24079 - Moving
+- Start Time : 25-05-18, 21:59
+- End Time : 25-05-18, 22:01
+- Try : 1
+- Category : simple, basic, mathematics, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	int x, y, z;
+	cin >> x >> y >> z;
+
+	cout << (x + y <= z);
+
+	return 0;
+}
+
+// Finish
