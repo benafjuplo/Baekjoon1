88483972105cc700b58a6f0cc435ba0bd2a3ba64
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-05-24 21:30:43 +0900

    [Bronze II] Title: 피자는 나눌 수록 커지잖아요, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17946. 피자는 나눌 수록 커지잖아요/피자는 나눌 수록 커지잖아요.cc
@@ -0,0 +1,36 @@
+/*
+- Title : 17946 - Pizza expands as you split it
+- Start Time : 25-05-24, 21:15
+- End Time : 25-05-24, 21:17
+- Try : 2(1 : forget newline)
+- Category : mathematics
+- Language : C++20
+*/
+
+/* 한국어 주석
+항상, 자를 때 새로 생기는 최대 피자 조각 수와 주어야 하는 피자 조각 수가 같다.
+따라서 정답은 입력에 관계없이 항상 1이다.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	int T;
+	cin >> T;
+
+	while (T--) {
+		long long x;
+		cin >> x;
+		cout << 1 << "\n";
+	}
+
+	return 0;
+}
+
+// Finish
