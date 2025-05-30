4a1b1b9985989e79a03566179d52745ec5536eb1
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-05-30 21:09:05 +0900

    [Bronze IV] Title: FA, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14935. FA/FA.cc
@@ -0,0 +1,29 @@
+/*
+- Title : 14935 - FA
+- Start Time : 25-05-29, 23:30
+- End Time : 25-05-29, 23:31
+- Try : 1
+- Category : mathematics, string
+- Language : C++20
+*/
+
+/* 한국어 주석
+이 문제는 어떤 입력이든 "FA"이다.
+항상 자리수가 1이 되면 반복하게 된다.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	cout << "FA";
+
+	return 0;
+}
+
+// Finish
