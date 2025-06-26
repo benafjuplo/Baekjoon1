f459cffc6460e6d1852f1238b23d2c78dbf237a5
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-06-27 01:12:24 +0900

    [Bronze V] Title: Conveyor Belt Sushi, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.cc
@@ -0,0 +1,31 @@
+/*
+- Title : 32326 - Conveyor Belt Sushi
+- Start Time : 25-06-27, 01:08
+- End Time : 25-06-27, 01:10
+- Try : 1
+- Category : mathematics, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	cout << a * 3 + b * 4 + c * 5;	
+
+	return 0;
+}
+
+// Finish
