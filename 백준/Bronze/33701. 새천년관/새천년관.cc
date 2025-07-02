ea7255afec8aa6ef47dcfa0ddca9f23ce20ea9be
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-07-02 20:42:26 +0900

    [Bronze IV] Title: 새천년관, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33701. 새천년관/새천년관.cc
@@ -0,0 +1,28 @@
+/*
+- Title : 33701 - New Millennium Hall
+- Start Time : 25-07-02, 20:39
+- End Time : 25-07-02, 20:40
+- Try : 1
+- Category : simple
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	cout << "nkukugwan";
+
+	return 0;
+}
+
+// Finish
