773b2baad056d4961352cc903ecab8154e773af9
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-08-03 21:39:26 +0900

    [Bronze V] Title: Just Round Down, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32458. Just Round Down/Just Round Down.cc
@@ -0,0 +1,28 @@
+/*
+- Title : 32458 - Just Round Down
+- Start Time : 25-08-03, 21:36
+- End Time : 25-08-03, 21:38
+- Try : 1
+- Category : implementation
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	int x;
+	cin >> x;
+	cout << x;
+
+	return 0;
+}
