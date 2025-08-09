635999509045dc9cdfd8951448fad65e75e27452
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-08-10 01:27:20 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 580 ms, Memory: 41096 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.py
@@ -0,0 +1,15 @@
+# ------------------------------------------------------------------------------
+# - Title       : 13277 - Multiplication of large numbers 
+# - Start Time  : 25-08-10, 01:05
+# - End Time    : 25-08-10, 01:15
+# - Try         : 1
+# - Category    : mathematics, elementary arithmetic, arbitrary-precision arithmetic
+# - Language    : Python 3
+# ------------------------------------------------------------------------------
+
+""" 한국어 주석
+Python에서는 큰 수의 곱셈을 단순 곱하기로 처리할 수 있다.
+"""
+
+x,y = input().split()
+print(int(x) * int(y))
