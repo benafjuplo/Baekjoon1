f75ce4680e11b0e68f88e5ae01750badff3294e1
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-08-11 00:26:54 +0900

    [Bronze V] Title: A+B - 9, Time: 136 ms, Memory: 39188 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.py
@@ -0,0 +1,15 @@
+# ------------------------------------------------------------------------------
+# - Title       : 15740 - A+B - 9
+# - Start Time  : 25-08-11, 00:17
+# - End Time    : 25-08-11, 00:20
+# - Try         : 1
+# - Category    : mathematics, elementary arithmetic, arbitrary-precision arithmetic
+# - Language    : Python 3
+# ------------------------------------------------------------------------------
+
+""" 한국어 주석
+Python은 단순 덧셈 연산으로 큰 수를 처리할 수 있다.
+"""
+
+x, y = map(int, input().split())
+print(x + y)
