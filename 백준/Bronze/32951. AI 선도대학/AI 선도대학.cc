eeac9d66f07f797c4958001b42172fdbae4de7a8
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-08-18 02:13:55 +0900

    [Bronze V] Title: AI 선도대학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.cc
@@ -0,0 +1,31 @@
+/*
+- Title : 32951 - AI Leading University
+- Start Time : 25-08-18, 02:11
+- End Time : 25-08-18, 02:12
+- Try : 1
+- Category : mathematics, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략 
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	cout << n - 2024;
+
+	return 0;
+}
+
+// Finish
