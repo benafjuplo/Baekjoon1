d01a1e3a34e6ec3cc99194d54ad407ba6af87dc5
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-10-19 05:52:55 +0900

    [Bronze V] Title: E-Days Ore Cart Pull, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34446. E－Days Ore Cart Pull/E－Days Ore Cart Pull.cc
@@ -0,0 +1,31 @@
+/*
+- Title : 34446 - E-Days Ore Cart Pull
+- Start Time : 25-10-19, 05:49
+- End Time : 25-10-19, 05:51
+- Try : 1
+- Category : mathematics, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	int m;
+	cin >> m;
+
+	cout << m * 2;
+
+	return 0;
+}
+
+// Finish
