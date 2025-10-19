e5019af7981dda14f28575c375f6cf8399e5db65
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-10-20 03:33:21 +0900

    [Bronze V] Title: M-Climb, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34411. M－Climb/M－Climb.cc
@@ -0,0 +1,31 @@
+/*
+- Title : 34411 - M-Climb
+- Start Time : 25-10-20, 02:07
+- End Time : 25-10-20, 02:08
+- Try : 1
+- Category : mathematics, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	int m, n;
+	cin >> m >> n;
+
+	cout << m * n;
+
+	return 0;
+}
+
+// Finish
