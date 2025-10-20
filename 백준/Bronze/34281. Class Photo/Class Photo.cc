b9080133840b556af4f37537ffa335c1fbf296f6
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-10-21 05:28:40 +0900

    [Bronze V] Title: Class Photo, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34281. Class Photo/Class Photo.cc
@@ -0,0 +1,30 @@
+/*
+- Title : 34281 - Class Photo
+- Start Time : 25-10-21, 05:08
+- End Time : 25-10-21, 05:09
+- Try : 1
+- Category : mathematics, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	int w, l;
+	cin >> w >> l;
+	cout << w * l;
+
+	return 0;
+}
+
+// Finish
