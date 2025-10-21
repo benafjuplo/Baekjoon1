6ec898b509628275f8cf4af3a3186410f08fec62
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-10-22 05:06:57 +0900

    [Bronze V] Title: Hack-a-Holics, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34443. Hack－a－Holics/Hack－a－Holics.cc
@@ -0,0 +1,31 @@
+/*
+- Title : 37773 - Hack-a-Holics
+- Start Time : 25-10-22, 05:02
+- End Time : 25-10-22, 05:03
+- Try : 1
+- Category : mathematics, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	int n, c, p;
+	cin >> n >> c >> p;
+
+	cout << n * p;
+
+	return 0;
+}
+
+// Finish
