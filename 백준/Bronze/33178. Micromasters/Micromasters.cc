66c49c94e5fd15dfd6d088608877a90e1e4d7adb
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-10-23 05:53:06 +0900

    [Bronze V] Title: Micromasters, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33178. Micromasters/Micromasters.cc
@@ -0,0 +1,31 @@
+/*
+- Title : 33178 - Micromasters
+- Start Time : 25-10-23, 05:49
+- End Time : 25-10-23, 05:51
+- Try : 1
+- Category : mathematics, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	cout << n / 10 << '\n';
+
+	return 0;
+}
+
+// Finish
