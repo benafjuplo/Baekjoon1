f4b6d82f295704eb01bcbe4f55db6060c022fea5
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-10-24 21:35:58 +0900

    [Bronze V] Title: Parking Pandemonium, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34297. Parking Pandemonium/Parking Pandemonium.cc
@@ -0,0 +1,31 @@
+/*
+- Title : 34297 - Parking Pandemonium
+- Start Time : 25-10-24, 20:20
+- End Time : 25-10-24, 20:21
+- Try : 1
+- Category : mathematics, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	int m, k, c;
+	cin >> m >> k >> c;
+
+	cout << m * c;
+
+	return 0;
+}
+
+// Finish
