55aef58679a607c58c4a640b462bd2d83c0a1a0a
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-10-25 17:42:20 +0900

    [Bronze V] Title: The World Responds, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23234. The World Responds/The World Responds.cc
@@ -0,0 +1,28 @@
+/*
+- Title : 23234 - The World Responds
+- Start Time : 25-10-25, 17:37
+- End Time : 25-10-25, 17:37
+- Try : 1
+- Category : implementation
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	cout << "The world says hello!" << '\n';
+
+	return 0;
+}
+
+// Finish
