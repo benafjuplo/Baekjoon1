f3b5930a5f44b705023c156f4c5c5d1853aee1d2
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-10-27 21:23:45 +0900

    [Bronze V] Title: 徒競走 (Footrace), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33165. 徒競走 （Footrace）/徒競走 （Footrace）.cc
@@ -0,0 +1,31 @@
+/*
+- Title : 33165 - Footrace
+- Start Time : 25-10-26, 19:15
+- End Time : 25-10-26, 19:15
+- Try : 1
+- Category : mathematics, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	int t, v;
+	cin >> t >> v;
+
+	cout << t * v;
+
+	return 0;
+}
+
+// Finish
