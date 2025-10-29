d248036e5ad6b82735ac1054213166fc27c76e90
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-10-29 20:46:29 +0900

    [Bronze V] Title: 鉛筆 2 (Pencils 2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33161. 鉛筆 2 （Pencils 2）/鉛筆 2 （Pencils 2）.cc
@@ -0,0 +1,31 @@
+/*
+- Title : 33161 - Pencils 2
+- Start Time : 25-10-29, 19:52
+- End Time : 25-10-29, 19:54
+- Try : 1
+- Category : mathematics, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	int a;
+	cin >> a;
+
+	cout << a / 5;
+
+	return 0;
+}
+
+// Finish
