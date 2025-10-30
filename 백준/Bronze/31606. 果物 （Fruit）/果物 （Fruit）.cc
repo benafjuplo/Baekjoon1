0406bd31597b9914130f6ca8fa9a8f6105df48f2
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-10-30 22:13:33 +0900

    [Bronze V] Title: 果物 (Fruit), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31606. 果物 （Fruit）/果物 （Fruit）.cc
@@ -0,0 +1,31 @@
+/*
+- Title : 31606 - Fruit
+- Start Time : 25-10-29, 20:08
+- End Time : 25-10-29, 20:09
+- Try : 1
+- Category : mathematics, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	int x, y;
+	cin >> x >> y;
+
+	cout << x + y + 3 << "\n";
+
+	return 0;
+}
+
+// Finish
