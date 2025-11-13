c7155d30ff6d6e80f576bcfa00e04cbda3ad482c
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-11-14 01:23:47 +0900

    [Bronze V] Title: 긴자리 계산, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.py
@@ -0,0 +1,18 @@
+# ------------------------------------------------------------------------------
+# - Title       : 2338 - Long-digit calculation
+# - Start Time  : 25-11-12, 16:46
+# - End Time    : 25-11-12, 16:50
+# - Try         : 1
+# - Category    : big integer, elementary arithmetic
+# - Language    : Python 3
+# ------------------------------------------------------------------------------
+
+""" 한국어 주석
+파이썬은 기본적으로 큰 정수를 지원하므로, 그냥 int로 변환해서 연산하면 된다.
+"""
+
+a = input()
+b = input()
+print(int(a) + int(b))
+print(int(a) - int(b))
+print(int(a) * int(b))
