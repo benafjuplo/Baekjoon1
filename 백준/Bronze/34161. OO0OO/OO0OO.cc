619668c9f178181ff75dbb3d202bb19c9334e551
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-11-21 19:19:09 +0900

    [Bronze IV] Title: OO0OO, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/34161. OO0OO/OO0OO.cc
@@ -0,0 +1,31 @@
+/*
+- Title : 34161 - OO0OO
+- Start Time : 25-11-21, 03:34
+- End Time : 25-11-21, 03:35
+- Try : 1
+- Category : implementation, ad hoc, obfuscation
+- Language : C++20
+*/
+
+/* 한국어 주석
+문제가 난독화되어 있고 넌센스 문제이다.
+입력에 상관없이 -1을 10000번 줄마다 출력하면 된다.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	for (int i = 0; i < 10000; ++i) {
+		cout << -1 << "\n";
+	}
+
+	return 0;
+}
+
+// Finish
