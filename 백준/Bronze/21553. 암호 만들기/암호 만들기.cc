a66a423152923b1a3061dba6fc9d9afacba7cc87
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-12-01 07:12:42 +0900

    [Bronze III] Title: 암호 만들기, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21553. 암호 만들기/암호 만들기.cc
@@ -0,0 +1,32 @@
+/*
+- Title : 21553 - Creating a password
+- Start Time : 25-12-01, 05:50
+- End Time : 25-12-01, 05:52
+- Try : 1
+- Category : string, ad-hoc
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	string a, p;
+	cin >> a >> p;
+
+	cout << p;
+
+	return 0;
+}
+
+// Finish
