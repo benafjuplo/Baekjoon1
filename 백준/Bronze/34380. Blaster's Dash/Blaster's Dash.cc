8594d869966aea3464f4ea6994d8d98992fd52b0
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2025-12-23 19:24:05 +0900

    [Bronze IV] Title: Blaster's Dash, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34380. Blaster's Dash/Blaster's Dash.cc
@@ -0,0 +1,32 @@
+/*
+- Title : 34380 - Blaster's Dash
+- Start Time : 25-12-23, 18:58
+- End Time :
+- Try : 1
+- Category : mathematics, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	int m, v;
+	cin >> m >> v;
+	
+	int n;
+	cin >> n;
+
+	cout << (n + 10 + 10) * 2;
+
+	return 0;
+}
