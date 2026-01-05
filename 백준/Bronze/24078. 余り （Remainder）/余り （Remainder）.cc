d6cfef9f89a929e89c9020a412a1289399bff64b
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2026-01-06 05:33:18 +0900

    [Bronze V] Title: 余り (Remainder), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24078. 余り （Remainder）/余り （Remainder）.cc
@@ -0,0 +1,29 @@
+/*
+- Title : 24078 - Remainder
+- Start Time : 26-01-06, 05:31
+- End Time : 26-01-06, 05:32
+- Try : 1
+- Category : mathematics, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	int x;
+	cin >> x;
+
+	cout << x % 21;
+
+	return 0;
+}
