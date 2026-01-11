7e05b9f95ecc1fc6e3421f6534bf42a7cbb43329
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2026-01-12 03:52:13 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.cc
@@ -0,0 +1,31 @@
+/*
+- Title : 31610 - Drops Packing
+- Start Time : 26-01-12, 03:37
+- End Time : 26-01-12, 03:39
+- Try : 1
+- Category : mathematics, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	cout << a * b + c;
+
+	return 0;
+}
+
+// Finish
