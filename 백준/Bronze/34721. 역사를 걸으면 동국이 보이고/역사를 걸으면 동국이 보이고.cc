90add9891329ef5d078b8d42eee84aed06b3cd4d
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2026-01-25 18:56:39 +0900

    [Bronze V] Title: 역사를 걸으면 동국이 보이고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34721. 역사를 걸으면 동국이 보이고/역사를 걸으면 동국이 보이고.cc
@@ -0,0 +1,31 @@
+/*
+- Title : 34721 - Walking Through History, Seeing Dongguk
+- Start Time : 26-01-25, 18:52
+- End Time : 26-01-25, 18:54
+- Try : 1
+- Category : implementation
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; ++i) {
+		cout << "I love DGU" << '\n';
+	}
+
+	return 0;
+}
