9abb5dfaaaf9f2e7d3dd2b33a3a276be3578c89e
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2026-02-22 23:13:48 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cc
@@ -0,0 +1,29 @@
+/*
+- Title : 30030 - Find the Price of Sweet Corn
+- Start Time : 26-02-22, 22:56
+- End Time : 26-02-22, 22:58
+- Try : 1
+- Category : mathematics, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	int b;
+	cin >> b;
+
+	cout << b * 10 / 11;
+
+	return 0;
+}
