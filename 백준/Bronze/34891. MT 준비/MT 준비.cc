b4f612432fd7f604b0f1ae160c8566f2e6d1dd9e
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2026-03-06 18:15:07 +0900

    [Bronze IV] Title: MT 준비, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34891. MT 준비/MT 준비.cc
@@ -0,0 +1,29 @@
+/*
+- Title : 34891 - Preparing for MT
+- Start Time : 26-03-06, 17:36
+- End Time : 26-03-06, 17:38
+- Try : 1
+- Category : mathematics, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	int n, m;
+	cin >> n >> m;
+
+	cout << n / m + (bool)(n % m) << "\n";
+
+	return 0;
+}
