3e95d8442a29871c6d901e06b85f638cf2985693
benafjuplo <145705791+benafjuplo@users.noreply.github.com>
2026-03-15 10:13:45 +0900

    [Bronze III] Title: 얼룩말과 사자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34759. 얼룩말과 사자/얼룩말과 사자.cc
@@ -0,0 +1,29 @@
+/*
+- Title : 34759 - Zebras and Lions
+- Start Time : 26-03-15, 09:49
+- End Time : 26-03-15, 09:51
+- Try : 1	
+- Category : mathematics, elementary arithmetic
+- Language : C++20
+*/
+
+/* 한국어 주석
+설명 생략.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//cout.tie(0);
+
+	unsigned long long n;
+	cin >> n;
+
+	cout << 2 * n;
+
+	return 0;
+}
